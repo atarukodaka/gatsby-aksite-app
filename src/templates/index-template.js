c9ab93ec4ec830d2b5fc21e162da9000cc4214c2
Ataru KODAKA <ataru.kodaka@gmail.com>
2021-02-08 17:02:07 +0900

    pagination

--- src/templates/index-template.js
+++ src/templates/index-template.js
@@ -1,18 +1,36 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 import Layout from "../components/layout.js"
-import { graphql } from "gatsby"
 import { PostExcerpt } from "../components/post.js"
 
 
-const IndexTemplate = ( { data } ) => {
+export const data = graphql`
+  query ($skip: Int!, $limit: Int!){
+    allMdx (skip: $skip, limit: $limit){
+      nodes {
+        frontmatter { title, date(formatString: "YYYY-MM-DD") }
+        excerpt(truncate: true)
+        fields { slug, directory }
+      }
+    }
+  }
+`
+
+const IndexTemplate = ( { data, pageContext } ) => {
+  const { previousPagePath, nextPagePath } = pageContext;
   console.log(data) 
   return (
     <Layout>      
-    {
-      data.allMdx.nodes.map(node => (
-          <PostExcerpt node={node} key={node.id}/>
-      ))
-    }
+      {
+        data.allMdx.nodes.map(node => (
+            <PostExcerpt node={node} key={node.id}/>
+        ))
+      }
+      
+      <div>
+        {previousPagePath ? <Link to={previousPagePath}>Prev Page</Link> : null }
+			  {nextPagePath ? <Link to={nextPagePath}>Next Page</Link> : null }        
+      </div>
     </Layout>    
   )
 }
