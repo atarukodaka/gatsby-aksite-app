7a309e35de501aebae73b087589f956a31ee31cc
Ataru KODAKA <ataru.kodaka@gmail.com>
2021-02-12 21:33:34 +0900

    sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,13 +1,6 @@
 import React from "react"
 import { useStaticQuery, Link, graphql } from "gatsby"
-//import SidebarStyles from './sidebar.module.css'
 import './sidebar.module.css'
-
-const uniq_directories = ( nodes ) => {
-    return [...new Set(nodes.map ( 
-        node => node.fields.directory))
-    ].filter(v=>v).sort()
-}
 
 const Sidebar = () => {    
     const data = useStaticQuery(
@@ -19,8 +12,15 @@ const Sidebar = () => {
                         author
                     }                    
                 }
-                allMdx(
-                    sort: {fields: frontmatter___date, order: DESC},
+
+                directories: allMdx(sort: {fields: fields___directory, order: ASC}, filter: {fields: {directory: {ne: ""}}}) {
+                    group(field: fields___directory) {
+                      directory: fieldValue
+                    }
+                }
+                recentPosts: allMdx(
+                    limit: 10,
+                    sort: {fields: frontmatter___date, order: DESC}
                     ) {
                     nodes {
                         frontmatter { title }
@@ -29,7 +29,7 @@ const Sidebar = () => {
                         id
                     }
                 }
-                allSitePage(sort: {fields: context___fromDate, order: DESC}, 
+                monthlyArchives: allSitePage(sort: {fields: context___fromDate, order: DESC}, 
                     filter: {context: {archive: {eq: "monthly"} }}){
                     
                     nodes {
@@ -57,7 +57,7 @@ const Sidebar = () => {
             <h3>Recent Posts</h3>
             <ul>
             {
-                data.allMdx.nodes.slice(0, 10).map(node => (
+                data.recentPosts.nodes.map(node => (
                     <li key={node.id}>
                         <Link to={'/' + node.slug}>{node.frontmatter.title}</Link>
                     </li>
@@ -67,7 +67,7 @@ const Sidebar = () => {
             <h3>Directories</h3>
             <ul>
             {
-                uniq_directories(data.allMdx.nodes).map(directory => (
+                data.directories.group.map( ({directory}) => (
                     <li key={directory}>
                         <Link to={'/' + directory}>{directory}</Link>
                     </li>
@@ -78,7 +78,7 @@ const Sidebar = () => {
             <h3>Monthly Archives</h3>
             <ul>
             {
-                data.allSitePage.nodes.map(node => (
+                data.monthlyArchives.nodes.map(node => (
                     <li key={node.id}>
                         <Link to={node.path}>{node.context.year}/{node.context.month}</Link>
                     </li>
