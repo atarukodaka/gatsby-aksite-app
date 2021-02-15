f41d0ea3a811907a4fa39d6111266de8d67a8894
Ataru KODAKA <ataru.kodaka@gmail.com>
2021-02-15 19:34:59 +0900

    add-monthly-archives

---
+++ src/pages/archives.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MonthlyArchives from '../components/monthly_archives'
+import Layout from '../components/layout'
+
+export default () => {
+    return (
+        <Layout>
+            <h3>Monthly Archives</h3>
+            <MonthlyArchives/>
+        </Layout>
+    )
+}
+
