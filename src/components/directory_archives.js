96d87c599b79c90663e14bc8126c3fe8f1ba1a96
Ataru KODAKA <ataru.kodaka@gmail.com>
2021-02-23 13:29:45 +0900

    directory-archive

--- src/components/directory_archives.js
+++ src/components/directory_archives.js
@@ -7,9 +7,17 @@ import { Button } from '@material-ui/core'
 const ListToTree = require('list-to-tree')
 
 const DirectoryArchives = () => {
-    const { directoryArchives } = useStaticQuery(
+
+    const data = useStaticQuery(
         graphql`
         {
+            mdxPages: allMdx {
+                nodes {
+                    id, slug
+                    fields { directory, directory_name }
+                }
+            }
+   
         directoryArchives: allSitePage(filter: {context: {archive: {eq: "directory"}}}) {
             nodes {
               id
@@ -24,7 +32,29 @@ const DirectoryArchives = () => {
         }
         `)
 
+
     const list = []
+    data.mdxPages.nodes.filter(v=>v.fields.directory != "").map(node => {
+        const directory = node.fields.directory
+        const item = list.find(v => v.name == directory)
+        if (item === undefined){
+            const parts = directory.split('/')
+            //const label = parts.pop() || directory
+            const label = node.fields.directory_name.split('/').pop()
+            parts.pop()
+            const parent_dir = parts.join('/')
+            const parent = list.find(v => v.name == parent_dir)
+            const parent_id = (parent) ? parent.id : 0
+
+            list.push({ id: directory, parent: parent_id, name: directory, label: label, totalCount: 0 })
+        }
+    })
+    
+    list.forEach(node=>{
+        const re = new RegExp(`^${node.name}`)
+        node.totalCount = data.mdxPages.nodes.filter(v=> re.test(v.fields.directory)).length
+    })
+    /*
     directoryArchives.nodes.forEach(node => {
         const parts = node.context.directory.split('/')
         const label1 = parts.pop() || node.context.directory
@@ -35,6 +65,7 @@ const DirectoryArchives = () => {
 
         list.push({ id: node.context.directory, parent: parent_id, name: node.context.directory, label: label, totalCount: node.context.count })
     })
+    */
     const tree = new ListToTree(list).GetTree()
     return (
         <Tree nodes={tree} />
@@ -42,44 +73,19 @@ const DirectoryArchives = () => {
 }
 
 const Tree = ({ nodes }) => {
-    /*                        */
 
-    /*
-                    
-*/
     return (
         <ul className={styles.tree}>
             {
                 nodes.map(v => (
-                        <li key={v.id} className="directory">
-                            <Link to={'/' + v.name}>{v.label || v.name} ({v.totalCount})</Link>                        
-                            { v.child && (<Tree nodes={v.child}></Tree>)}
-                        </li>
-                    )
+                    <li key={v.id} className="directory">
+                        <Link to={'/' + v.name}>{v.label || v.name} ({v.totalCount})</Link>
+                        { v.child && (<Tree nodes={v.child}></Tree>)}
+                    </li>
+                )
                 )
             }
         </ul>)
 }
-/*
-const handleClick = (node) => {
-    //alert(node.name)
-    //navigate('/' + node.name)
-    navigate(node.path)
-}
-const TreeNodes = ({ nodes }) => {
-    return (
-        <div>
-        {
-            nodes.map(v => (
-                    <TreeItem nodeId={v.name} label={v.label + ' ('+ v.totalCount+ ')'} onLabelClick={()=> { handleClick(v)}}>
-                    { ( v.child ) ? <TreeNodes nodes={v.child}/> : '' }
-                    </TreeItem>
-                )
-            )
-        }
-        </div>
-    )
-}
-*/
 
 export default DirectoryArchives
